dd38ba0d38b111a31eec4b8c706b316c223f0fd3
FedorTregubov <f.a.tregubov@yandex.ru>
2022-05-01 00:00:07 +0200

    added unit-tests

---
+++ services/SpeechEvaluationService.spec.ts
@@ -0,0 +1,45 @@
+import { SpeechEvaluationService } from './SpeechEvaluationService';
+
+describe('SpeechEvaluationService', () => {
+  describe.skip('SpeechEvaluationService.getByMostSpeechesFromSpecialYear', () => {
+    it.todo('input obj should be the same after method call', () => {
+    });
+
+    it.todo('return null, if empty obj passed', () => {
+    });
+
+    it.todo('return null, if there is no speeches in specified year', () => {
+    });
+
+    it.todo('return speaker name, if there is most person with speeches in specified year', () => {
+    });
+  });
+
+  describe.skip('SpeechEvaluationService.getByMostPublicTopic', () => {
+    it.todo('input obj should be the same after method call', () => {
+    });
+
+    it.todo('return null, if empty obj passed', () => {
+    });
+
+    it.todo('return null, if there is no speeches in specified topic', () => {
+    });
+
+    it.todo('return speaker name, if there is most person with speeches in specified topic', () => {
+    });
+  });
+
+  describe.skip('SpeechEvaluationService.getByWordsCount', () => {
+    it.todo('input obj should be the same after method call', () => {
+    });
+
+    it.todo('return null if empty obj passed', () => {
+    });
+
+    it.todo('return least wordy person', () => {
+    });
+
+    it.todo('return most wordy person', () => {
+    });
+  });
+});
